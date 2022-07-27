3eec068edaf26806cea033aa24728a994c6a6b68
Mukesh-areo <mukeshbabu447@.com>
2022-07-27 17:39:48 +0530

    c

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+
 from flask import Flask
 
 app=Flask(__name__)
 
+
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machining project"
+    return "CI CD pipeline has been established."
 
-if __name__=='__main__':
+
+if __name__=="__main__":
     app.run(debug=True)
